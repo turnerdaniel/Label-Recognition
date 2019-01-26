a01cda858945b56a6e2efd45b2056267039344f9
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2019-01-26 19:47:42 +0000

    Update script.m

--- script.m
+++ script.m
@@ -8,8 +8,6 @@ clear; close all; clc;
 %img = imread('img/image123.jpeg');
 I = imread('img/20 NOV(2325).jpeg');
 
-[height, width] = size(I);
-
 %% Convert to greyscale
 %Check if image is RGB denoted by being 3D array
 if size(I,3) > 0
@@ -18,44 +16,93 @@ else
     grey = I;
 end
 
+[height, width] = size(grey);
+
 %% Increase contrast/clarity?
 
+%use imtophat to remove uneven illumination!
+
+%Perform Contrast Limited Adaptive Histogram Equalisation to help boost
+%contrast between text and BG
 clahe = adapthisteq(grey);
 
-figure, subplot(1,2,1), imshow(grey), title('greyscale image');
+figure, subplot(1,2,1), imshow(grey), title('Greyscale Image');
 subplot(1,2,2), imshow(clahe), title('Contrast Limited Adaptive Hist EQ');
 
 %% Maximally Stable Extremal Regions (MSER)
 
-mserRegions = detectMSERFeatures(clahe,'RegionAreaRange',[150 1500]);
+%Maybe change thresholdDelta(0.8 - 4)?
+mserRegions = detectMSERFeatures(clahe, 'RegionAreaRange', [150 1500]);
 mserPixels = vertcat(cell2mat(mserRegions.PixelList));
 
 figure, imshow(I);
 hold on;
 plot(mserRegions, 'showPixelList', true,'showEllipses', false);
-title('MSER:');
+title('MSER Regions');
 hold off;
 
-%Convert MSER pixel lists to a binary mask
-% mserBW = false(size(grey));
-% ind = sub2ind(size(mserMask), mserPixels(:,2), mserPixels(:,1));
-% mserMask(ind) = true;
+%% Edge Detection using Canny Edge Detector (edge-enhanced MSER)%
+%
+% %Reduce blur from MSER is using edge detection to remove overlapping
+% %pixels (edge-enhanced MSER)
+% 
+% %Canny was much better at finding the edge of text 
+% edgeMask = edge(clahe, 'canny');
+% 
+% figure, imshow(edgeMask), title('Edge Image');
+% 
+% %Convert MSER pixels to binary image
+% mserBW = false(height, width);
+% %Convert img coords to linear image index
+% ind = sub2ind(size(mserBW), mserPixels(:,2), mserPixels(:,1));
+% mserBW(ind) = true;
+% 
+% Intersection = edgeMask & mserBW; 
+% 
+% %Use Sobel to get gradien and directions
+% [, gTheta] = imgradient(clahe);
+% 
+% %Need to Grow Edges using direction of gradient
+% 
+% %You must specify if the text is light on dark background or vice versa
+% %gradientGrownEdgesMask = helperGrowEdges(Intersection, gTheta, 'LightTextOnDark');
+% %figure; imshow(gradientGrownEdgesMask); title('Edges grown along gradient direction')
 
-%% Edge Detection using Canny Edge Detector
+%% Remove Unlikely Candidates using Region Properties
 
-%Canny was much better at finding the edge of text 
-edgeMask = edge(grey, 'canny');
+% Eccentricity = similar to a line segment (1)
+% Euler Number = Don't have many holes (max 2 | B)
+% Aspect Ratio = mostly square
+% Extent = have very high or very low occupation of bounding box (O vs l)
 
-figure, imshow(edgeMask), title('Edge Image');
+%bwareaopen = letters arent too big/too small
 
-% Find intersection between edges and MSER regions
-% Intersection = edgeMask & mserMask; 
-% figure; imshowpair(edgeMask, Intersection, 'montage'); 
-% title('Canny edges and intersection of canny edges with MSER regions')
+%% Stroke Width Transform
 
+% helperStrokeWidth() - or could make own:
+% https://github.com/tanmayGIT/ICDAR-2015-DTW/blob/master/helperStrokeWidth.m
 
+%% Region Growing based on colour?
+
+% text will always be black
+% Hard to find start condition
+
+%% Connected text
+
+%1 = calculate bounding boxes and expand by small amount. Overlap is the
+%same text region.
+
+%2 = peform morphological operation on text to grow/join letters. Then
+%group into bounding boxes
 
 %% Perform Optical Character Recognition (OCR)
 
 ocrtxt = ocr(I);
 [ocrtxt.Text]
+
+%improve by creating a ocr characterSet to limit the possible characters to
+%letters/numbers found in dates (1234567890abcdefghij_lmnop_rstuv__y_)
+
+%Change 'textLayout' to 'Block'/'line' may help
+%Perform morphology to help thin-out connected characters
+%Make sure letters are of adequate size (>20px)
