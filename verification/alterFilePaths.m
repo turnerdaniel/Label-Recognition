09ae8e4d5b770356f3a41b05bf3ac640ae426895
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2019-03-07 01:32:16 +0000

    Chnaged function to accept handle objects

--- verification/alterFilePaths.m
+++ verification/alterFilePaths.m
@@ -1,18 +1,30 @@
-function gt = alterFilePaths(gTruth, newPath)
-%ALTERFILEPATHS changes the file paths for a groundTruth object
+function alterFilePaths(gTruth, newPath)
+%ALTERFILEPATHS Change file paths of a groundTruth object.
+%This is an alternative function to changeFilePaths() introduced in MATLAB 
+%2018b which is not installed on the University of Lincoln computers. 
+%
+% Syntax: alterFilePaths(gTruth, newPath)
+%
+% Inputs:
+%   gTruth  - Handle of groundTruth object that needs file paths changed.
+%             This is passed by reference.
+%   newPath - Filepath to new directory holding the dataset.
+%
+% See also: CHANGEFILEPATHS
 
-%supply:
-%gTruth object
-%filepath to dataset folder ending with '/' or '\'
-
+%Allocate variables to hold all filepaths
 length = size(gTruth.DataSource, 1);
 images = cell(length, 1);
 
 for i = 1:length
+    %Get filenames of images
     [~, name, ext] = fileparts(gTruth.DataSource{i});
+    %Append filenames to new path
     images{i} = fullfile(newPath, strcat(name, ext));
 end
 
+%Create new image source for groundTruth from new paths
 imageSource = groundTruthDataSource(images);
 
-gt = groundTruth(imageSource, gTruth.LabelDefinitions, gTruth.LabelData);
+%Alter groundTruth handle object with rectified path
+gTruth.DataSource = imageSource;
