d4587dfca6aec3af7b2e2fbfcc67308a6a55cbfd
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2019-04-11 09:45:31 +0100

    Updated Comments

--- verification/gatherMetrics.m
+++ verification/gatherMetrics.m
@@ -21,7 +21,8 @@
 %MINIMUM TIME = The fastest time the algorithm was completed
 %MAXIMUM TIME = The slowet time the algorithm was completed
 %STANDARD DEVIATION = The amount of varition in time complexity
-%STANDARD ERROR of the MEAN = 
+%STANDARD ERROR of the MEAN = How well the average time complexity
+%represents the population time complexity
 
 %Reset MATLAB environement
 clear; close all; clc;
@@ -80,73 +81,76 @@ parfor iteration = 1:imageCount
     %Load image from filepath resent in ground truth object
     I = imread(gTruth.DataSource.Source{iteration});
     
-    %% Convert to greyscale
+    %% Greyscale Conversion
     %Check if image is RGB denoted by being 3D array
     if size(I,3) > 0
+        %Convert to greyscale
         grey = rgb2gray(I);
     else
+        %Rename since already greyscale
         grey = I;
     end
     
     %Get dimensions of image
     [imageHeight, imageWidth] = size(grey);
     
-    %% Image Pre-processing - Remove Noise, Increase Contrast & Sharpness
+    %% Image Pre-processing - Noise Reduction, Increase Contrast & Sharpness
     
-    %Perform Linear Spatial Filtering to eliminate noise
+    %Perform Spatial Filtering to eliminate noise
     %Weiner removes gaussian & speckle noise while preserving edges by adapting
-    %smoothing amount
+    %smoothing amount for 3x3 neighbourhood
     greyWeiner = wiener2(grey, [3 3]);
     
-    %Perform Linear Contrast Stretching (Could change Gamma?)
+    %Perform Linear Contrast Stretching
     greyContrastStretch = imadjust(greyWeiner);
     
-    %Use unsharp masking to increase image sharpness
+    %Use Unsharp Masking to increase image sharpness
     greySharp = imsharpen(greyContrastStretch);
     
     %% Maximally Stable Extremal Regions (MSER)
     
-    %Detect MSER Regions
-    %RegionAreaRange = region min|max size (30 14000)
-    %ThresholdDelta = Step size between intensity threshold (2)
-    %MaxAreaVariation = max area variation between regions (0.25)
+    %Detect text using MSER
+    %RegionAreaRange = region min/max size (default: 30 14000)
+    %ThresholdDelta = Step size between intensity threshold (default: 2)
+    %MaxAreaVariation = max area variation between regions (default: 0.25)
     mserRegions = detectMSERFeatures(greySharp, 'RegionAreaRange', ...
         [150 1500], 'ThresholdDelta', 2.5, 'MaxAreaVariation', 0.2);
     
-    %Concatenate pixel coordinates as Nx2 matrix
+    %Concatenate pixel coordinates from MSER as Nx2 matrix
     mserPixels = vertcat(cell2mat(mserRegions.PixelList));
     
     %Initialise logical image with necessary dimensions
     mserBW = false(imageHeight, imageWidth);
     %Convert img co-ordinates to linear image indexes
     ind = sub2ind(size(mserBW), mserPixels(:,2), mserPixels(:,1));
-    %assign true to co-ordinates that match
+    %Set matching pixels to white
     mserBW(ind) = true;
     
-    %% Image Post-Processing - Opening,
+    %% Image Post-Processing - Opening, Removal of Small Objects, Region Filling
     
     %Define 3x3 square structuring element
     seSquare = strel('square', 3);
     
-    %Opening to remove small joins
+    %Perform opening to remove protrusions and small joins
     opened = imopen(mserBW, seSquare);
     
-    %Remove small blobs below 100 pixels
+    %Remove blobs with an area below 100 pixels
     clearNoise = bwareaopen(opened, 100);
-    %Close small holes by inverting image between foreground and background
+    %Fill small holes by inverting image so the background becomes foreground
     clearSmallHoles = ~bwareaopen(~clearNoise, 3);
     
     %% Connected Component Enhanced MSER (CCEMSER)
     
-    %Get the image containg solely the object and its bounding box
+    %Get the image solely containing each object and its bounding box
     mserStats = regionprops(clearSmallHoles, 'Image', 'BoundingBox');
-    %Initialise variables
+    %Initialise variables to hold total number of objects and output image
     totalObjects = size(mserStats, 1);
     CCadjustedImage = false(imageHeight, imageWidth);
     
+    %Loop through all objects in image
     for i = 1:totalObjects
-        %Get BBox and image
-        %Correct for slightly large BBox and round floating values
+        %Get bounding box and image for current object
+        %Correct for slightly large bbox by rounding co-ordinates
         imageBBox = ceil(mserStats(i).BoundingBox - [0, 0, 1, 1]);
         image = mserStats(i).Image;
         
@@ -156,15 +160,16 @@ parfor iteration = 1:imageCount
         %Threshold the cropped greyscale image using Otsu's Method
         binaryImage = imbinarize(greyImage);
         
-        %Perform CC analysis to find most likely text (BoW, DoL text)
+        %Identify connected components in the intersection between binary image
+        %and thresholded image
         ccReg = bwconncomp(image & binaryImage);
         ccInv = bwconncomp(image & ~binaryImage);
         
-        %Concatenate list to handle multiple objects in image
+        %Concatenate list to handle multiple objects in an image
         ccRegSize = size(cat(1, ccReg.PixelIdxList{:}), 1);
         ccInvSize = size(cat(1, ccInv.PixelIdxList{:}), 1);
         
-        %Check objects to find which has the fewest non-zero components which
+        %Check objects to find which has the fewest number of components above 0 which
         %will more than likely have the appropriate threshold
         if ccReg.NumObjects == 0 && ccInv.NumObjects ~= 0
             %Use inverse image
@@ -180,7 +185,7 @@ parfor iteration = 1:imageCount
             keepImage = image & ~binaryImage;
             %Edge case where they could have matching number of objects
         elseif ccInv.NumObjects == ccReg.NumObjects
-            %Choose threshold that procuces the most pixels
+            %Choose threshold that produces the most pixels
             if ccRegSize < ccInvSize
                 keepImage = image & ~binaryImage;
             else
@@ -196,61 +201,56 @@ parfor iteration = 1:imageCount
             imageBBox(1):imageBBox(1) + imageBBox(3)) = keepImage;
     end
     
-    %Remove small blobs
+    %Remove blobs with an area below 100 pixels
     clearNoise = bwareaopen(CCadjustedImage, 20);
-    %Close small holes by inverting image between foreground and background
+    %Fill small holes by inverting image so the background becomes foreground
     clearSmallHoles = ~bwareaopen(~clearNoise, 3);
     
-    %% Remove Unlikely Candidates using Region Properties
-    
-    % Eccentricity = similarity to line segment (1) or cirlce (0)
-    % Euler Number = Don't have many holes (max 2 | B)
-    % Aspect Ratio = mostly square (vertical & horizontal)
-    % Extent = have very high or very low occupation of bounding box (O vs l)
+    %% Geometric Filtering
     
     %Label the image and get the properties of each object
     mserLabel = bwlabel(clearSmallHoles);
     mserStats = regionprops(clearSmallHoles, 'BoundingBox', 'Eccentricity', ...
         'EulerNumber', 'Extent', 'Solidity');
     
-    %Calculte the maximum horizontal/vertical aspect ratio
+    %Calculte the maximum aspect ratio for horizontal and vertical direction
     bBoxes = vertcat(mserStats.BoundingBox);
     bbWidths = bBoxes(:, 3)';
     bbHeights = bBoxes(:, 4)';
     aspectRatio = max(bbWidths ./ bbHeights, bbHeights ./ bbWidths);
     
-    %Remove blobs that have more than 3 holes in them. Need to compensate for
-    %noise
+    %Keep blobs that have less than 3 holes in them. Need to compensate for noise
     validEulerNo = [mserStats.EulerNumber] >= -2;
-    %Remove blobs that are perfect lines (eg. barcodes)
+    %Keep blobs that are not perfect lines (eg. barcodes)
     validEccentricity = [mserStats.Eccentricity] < 0.99;
-    %Letters should have normal distribution of Area to BBox
+    %Keep blobs that fall within normal distribution of Area to BBox ratio
     validExtent = [mserStats.Extent] > 0.25 & [mserStats.Extent] < 0.9;
-    %The ratio between the region and the convex hull
+    %Keep blobs that do not have significant convex areas
     validSolidity = [mserStats.Solidity] > 0.4;
-    %Make use of vertical and horizontal aspect ratio to ensure shape are
-    %roughly square = 1
+    %Keep blobs that are not extremely elongated
     validAspectRatio = aspectRatio < 3;
     
     %Find the index of all objects that have valid properties
     keptObjects = find(validEulerNo & validEccentricity & validExtent & ...
         validSolidity & validAspectRatio);
-    %Return pixels that satisfy the similar property criteria for the image
+    %Return pixels that are at the valid indexes for the image
     keptObjectsImage = ismember(mserLabel, keptObjects);
     
     %% Stroke Width Transform
     
-    %Label the image and get the properties of each object
+    %Label the image and get the smallet image encapsulating each object
     swtLabel = bwlabel(keptObjectsImage);
     swtStats = regionprops(keptObjectsImage, 'Image');
     
-    %Initialise variables
+    %Initialise variables that hold the number of objects and their stroke
+    %width variations
     totalObjects = size(swtStats, 1);
     swtVariation = zeros(1, totalObjects);
     
     %Define the maximum variation in stroke width for letters
     swtVariationThresh = 0.4;
     
+    %Loop through all objects in image
     for i = 1:totalObjects
         %Get the image conting just the object
         object = swtStats(i).Image;
@@ -258,10 +258,10 @@ parfor iteration = 1:imageCount
         %Pad the image with 0's to avoid stroke width being affected by boundary
         paddedObject = padarray(object, [1 1]);
         
-        %Distance Transform
+        %Perform Distance Transform
         distanceTransform = bwdist(~paddedObject);
         
-        %Perform thinning until Skeleton
+        %Perform thinning until Skeleton is created
         skeletonTransform = bwmorph(paddedObject, 'thin', inf);
         
         %Retrieve the stroke width values for the image
@@ -271,27 +271,26 @@ parfor iteration = 1:imageCount
         swtVariation(i) = std(swtWidths)/mean(swtWidths);
     end
     
-    %Find stroke widths that are below the variation threshold
+    %Find valid stroke widths that are below the variation threshold
     validStrokeWidths = swtVariation < swtVariationThresh;
     %Find the index of these objects
     keptSWT = find(validStrokeWidths);
     %Create an image made of objects that are below the variation threshold
     keptSWTImage = ismember(swtLabel, keptSWT);
     
-    %% Rule-Based Candidate Text Grouping
+    %% Rule-Based Text Grouping
     
-    %Get the bounding box for each object
+    %Get the bounding box for each object and convert to usable coordinates
     textStats = regionprops(keptSWTImage, 'BoundingBox');
     textROI = vertcat(textStats.BoundingBox);
     
-    %Get bounding box sizes
+    %Seperate bounding box into seperate variables for manipulation
     roiX = textROI(:, 1);
     roiY = textROI(:, 2);
     roiW = textROI(:, 3);
     roiH = textROI(:, 4);
     
-    %Expand ROI by 2/3 the character width in horizontal direction since dates
-    %are typically vertically aligned
+    %Expand ROI by 2/3 the character width in horizontal direction
     expandedX = roiX - (roiW * (2/3));
     expandedW = roiW + ((roiW * (2/3)) * 2);
     
@@ -301,7 +300,7 @@ parfor iteration = 1:imageCount
     
     %Create expanded bounding boxes
     expandedTextROI = [expandedX, roiY, expandedW, roiH];
-
+    
     %Calculate the ratio of union between bounding boxes
     overlapRatio = bboxOverlapRatio(expandedTextROI, expandedTextROI, 'Union');
     overlapSize = size(overlapRatio, 1);
@@ -313,30 +312,33 @@ parfor iteration = 1:imageCount
     %that intersect
     labelledROI = conncomp(graph(overlapRatio));
     
-    %Ensure that there is similarity between connected letters
-    maxComponents = max(labelledROI);
+    %Find the total number of bounding boxes
+    totalComponents = max(labelledROI);
+    
     %loop through connected bounding boxes
-    for i = 1:maxComponents
+    for i = 1:totalComponents
         %find the index of connected bounding boxes
         connectedBoxes = find(labelledROI == i);
-        %Get the bounding box heights
+        %Get their heights
         heightOfBoxes = roiH(connectedBoxes);
         
-        %Ensure that joined regions have similar heights
+        %Calculate the average height of connected bounding boxes
         meanHeight = mean(heightOfBoxes);
         meanError = meanHeight/2;
         
-        %Create mask of bounding boxes that don't meet the criteria
+        %Find all bounding boxes that have a height that matches the criteria
         validBoxes = heightOfBoxes > meanHeight - meanError & heightOfBoxes < meanHeight + meanError;
+        %Get the index of all bounding boxes that don't match the criteria
         discardHeight = find(validBoxes == 0);
         
-        %Get the index of bounding boxes that don't meet the criteria
+        %Get bounding boxes that don't meet the criteria using index
         invalidHeight = connectedBoxes(discardHeight);
         
-        %Check that there the validHeights matrix is not empty
+        %Check that invalidHeights is not empty
         if (~isempty(invalidHeight))
             %Loop through invalid indexes
             for j = 1:size(invalidHeight, 2)
+                %Get the id of the bounding box with an invalid height
                 id = invalidHeight(j);
                 %Seperate the component into a new indices by creating
                 %a new 'label' above max value
@@ -377,10 +379,11 @@ parfor iteration = 1:imageCount
     %Remove single, unconnected bounding boxes for binary image
     removeROIImage = keptSWTImage;
     for i = 1:size(removePixelsROI, 1)
+        %Set pixels to 0 inside bounding boxes that need to be removed
         removeROIImage(removeMinY(i):removeMaxY(i), removeMinX(i):removeMaxX(i)) = 0;
     end
     
-    %Expand a the bounding box vertically to fully contain the text's height
+    %Expand the bounding box vertically to fully contain the text's height
     pixelExpansion = 2;
     expandedY = filteredTextROI(:, 2) - pixelExpansion;
     expandedH = filteredTextROI(:, 4) + (2 * pixelExpansion);
@@ -392,18 +395,19 @@ parfor iteration = 1:imageCount
     expandedFilteredTextROI = [filteredTextROI(:, 1), expandedY, ...
         filteredTextROI(:, 3), expandedH];
     
-    %% Perform Optical Character Recognition (OCR)
+    %% Orientation Correction & Optical Character Recognition (OCR)
     
     %Get text ROIs to perform OCR on
     ocrROI = [expandedFilteredTextROI(:, 1), expandedFilteredTextROI(:, 2), ...
         expandedFilteredTextROI(:, 3), expandedFilteredTextROI(:, 4)];
     
-    %Pre-allocate vectors & initialise variables
+    %Pre-allocate vectors & initialise variables to hold number of bboxes, OCR
+    %results and the sampling points for the line of best fit
     ROISize = size(ocrROI, 1);
     detectedText = strings(ROISize, 1);
     samplePoints = 10;
     
-    %Loop through candidate words
+    %Loop through text lines
     for i = 1:ROISize
         %Crop binary image using expanded bounding boxes
         ROI = imcrop(removeROIImage, [ocrROI(i, 1), ocrROI(i, 2), ocrROI(i, 3), ...
@@ -426,12 +430,12 @@ parfor iteration = 1:imageCount
         %Convert to 4xN matrix for operations
         letterBoxes = vertcat(ROIstats.BoundingBox);
         
-        %Get the centre of the bounding boxes. This is better than centroids as
-        %the centre of gravity is not the true centre.
+        %Get the centre of the bounding boxes. This is better than MATLAB centroids as
+        %they calculate true centre
         centreX = round(mean([letterBoxes(:, 1), letterBoxes(:, 1) + letterBoxes(:, 3)], 2));
         centreY = round(mean([letterBoxes(:, 2), letterBoxes(:, 2) + letterBoxes(:, 4)], 2));
         
-        %Calculate line of best fit coeffecient using the centre of letters
+        %Calculate line of best fit coeffecients using the centre of letters
         bestFit = polyfit(centreX, centreY, 1);
         
         %Create linearly spaced vector of 10 sample points from 1 to width of
@@ -447,8 +451,9 @@ parfor iteration = 1:imageCount
             xValues(samplePoints) - xValues(1));
         
         %Don't correct image if it is within 7.5 degrees of 0
-        %OCR is capable of reading letters most accurately at < 10 degree offset
+        %OCR is capable of reading letters most accurately at ~< 10 degree offset
         if (angle > 7.5 || angle < -7.5)
+            %Rotate the image by the angle using nearest negithbour interpolation
             ROI = imrotate(ROI, angle);
         end
         
@@ -463,10 +468,8 @@ parfor iteration = 1:imageCount
         detectedText(i) = ocrOutput.Text;
     end
     
-    %% Perform Text Matching using Regex
-    
-    %Formats found in more detail in the script...
-    
+    %% Date Matching
+        
     %Handles formats 1 and 6
     regexTextDate = '(\d{1,2})([\/\\\-\. ]|)(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)([\/\\\-\. ]|)((?:\d{2}){0,2})';
     %Handles formats 2
