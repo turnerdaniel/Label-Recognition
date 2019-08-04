ed43652b289b6ae1702f3695853a161cee314141
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2019-08-04 15:49:34 +0100

    Add preProcess and MSER methods

--- LabelRecogniser.m
+++ LabelRecogniser.m
@@ -6,11 +6,10 @@ classdef LabelRecogniser
     %manufacturers.
     
     properties (Access = public)
-        mserSigma = 0.3; % Sigma used for MSER
-        % Could put parameters in here (e.g. mser settings, etc.
+        image % Image used for recognition
+        thresholdDelta = 0.3; % Delta used for MSER
     end
     properties (Access = private)
-        image % Input image
         h % height of input image
         w % width of input image
     end
@@ -31,6 +30,8 @@ classdef LabelRecogniser
         function recogniseDates(obj, show)
             %recogniseDates Identify the position and textual representation of the dates shown within the image.
             img = convertGrey(obj);
+            img = preProcess(obj, img);
+            img = mser(obj, img);
             
             if (show == true)
                 imshow(img);
@@ -38,15 +39,44 @@ classdef LabelRecogniser
         end
     end
     methods (Access = private)
-        function grey = convertGrey(obj)
+        function out = convertGrey(obj)
             %convertGrey Convert an RGB image to greyscale
-            %   Detailed explanation goes here
             
+            %Maybe use image like other functions?
             if size(obj.image, 3) > 0
-                grey = rgb2gray(obj.image);
+                out = rgb2gray(obj.image);
             else
-                grey = img;
+                out = obj.image;
             end
+        end
+        
+        function out = preProcess(~, image)
+            %preProcess Perform image enhancements to improve text clarity
+            
+            %Perform Spatial Filtering to eliminate noise
+            noise = wiener2(image, [3 3]);
+            %Perform Linear Contrast Stretching
+            contrast = imadjust(noise);
+            %Use Unsharp Masking to increase image sharpness
+            out = imsharpen(contrast);
+        end
+        
+        function out = mser(obj, image)
+            %mser Detect text candiates using Maximally Stable Extremal Regions
+            
+            %Initialise logical image with necessary dimensions
+            out = false(obj.h, obj.w);
+            
+            %Could make parameters editable?
+            mser = detectMSERFeatures(image, 'RegionAreaRange', [150 1500], ...
+                'ThresholdDelta', 2.5, 'MaxAreaVariation', 0.2);
+            
+            %Concatenate pixel coordinates from MSER as Nx2 matrix
+            pixels = cell2mat(mser.PixelList);            
+            %Convert img co-ordinates to linear image indexes
+            ind = sub2ind([obj.h, obj.w], pixels(:,2), pixels(:,1));
+            %Set matching pixels to white
+            out(ind) = true;
         end
     end
 end
